8c83bd96c2cb76d084ef8b9897ffe52a5e6d7b03
Jiahao Zhu <jzhu620@gatech.edu>
2024-01-31 10:50:45 -0500

    first commit

---
+++ hello world.py
@@ -0,0 +1,34 @@
+#############################
+# Learn Python Coding in 2024
+# By Jarvis Zhu
+# Codemy.com
+# This is a 6400 test
+#############################
+
+
+# Clear the terminal screen
+import os
+os.system('clear')
+
+#############################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+############################
+
+
+
+first_name = 'Jarvis'
+age = 30 
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"	
+	}
+name = True
+
+print (name)
